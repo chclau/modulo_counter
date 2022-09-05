e05fedef1c03186d7bc90fd822c1b128256b85e8
Claudio Avi Chami <chclau@yahoo.com>
2022-09-05 22:10:33 +0300

    Update modulo_cnt.vhd

--- src/modulo_cnt.vhd
+++ src/modulo_cnt.vhd
@@ -1,3 +1,4 @@
+
 ------------------------------------------------------------------
 -- Name        : modulo_cnt.vhd
 -- Description : Modulo counter
@@ -30,29 +31,25 @@ entity modulo_cnt is
    );
 end modulo_cnt;
 
-
 architecture rtl of modulo_cnt is
-   signal   cnt       : unsigned(DATA_W-1 downto 0);
-   signal   zero_i   : std_logic;
+   signal   cnt      : unsigned(DATA_W-1 downto 0);
 
 begin 
-zero_i   <= '1' when cnt = 0 else '0';
-zero     <= zero_i;
+  zero  <= '1' when cnt = 0 else '0';
 
-counter_pr: process (clk, rst) 
-   begin 
-    if (rst = '1') then 
-        cnt       <= (others => '0');
-    elsif (rising_edge(clk)) then   
-      if (en = '1') then            -- is counting enabled?
-         if (zero_i = '1') then     -- check if counter reached zero
-            cnt   <= unsigned(max_cnt) - 1;     
-                                    -- reload with modulo value
-         else
-            cnt   <= cnt - 1;       -- decrement counter
-         end if;  
+  counter_pr: process (clk) 
+  begin
+    if (rising_edge(clk)) then   
+      if (rst = '1') then 
+        cnt  <= (others => '0');
+      elsif (en = '1') then            -- is counting enabled?
+        if (zero = '1') then         -- check if counter reached zero
+          cnt   <= unsigned(max_cnt) - 1;   -- reload with modulo_value-1
+        else
+          cnt   <= cnt - 1;       -- decrement counter
+        end if;  
       end if;  
     end if;
-end process;
+  end process counter_pr;
 
 end rtl;
